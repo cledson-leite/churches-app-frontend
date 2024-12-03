ca5ee89966ec54f26e0740880bafe2900c86d983
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2024-12-03 14:30:19 -0300

    feat: implement internationalization

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import pt from '@/shared/I18n/locales/pt.json';
+import { NextIntlClientProvider, useLocale } from 'next-intl';
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  const locales = {
+    pt: { ...pt },
+  };
+  const timeZone = 'America/Sao_Paulo';
+  const locale = useLocale();
+  const messages = locales[locale];
+  return (
+    <NextIntlClientProvider
+      timeZone={timeZone}
+      locale={locale}
+      messages={messages}
+    >
+      {children}
+    </NextIntlClientProvider>
+  );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import createIntlMiddleware from 'next-intl/middleware'
+
+export default createIntlMiddleware({
+  locales: ['pt', 'en'],
+  defaultLocale: 'pt',
+})
+
+export const config = {
+  // matcher: '/:locale*',
+  matcher: ['/', '/(pt|en)/:path*']
+}
