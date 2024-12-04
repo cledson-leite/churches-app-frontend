c5867fb86688f8af056d686e93517c195a33ea5d
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2024-12-04 18:08:59 -0300

    feat: componente header

--- src/shared/Routes/finance.ts
+++ src/shared/Routes/finance.ts
@@ -1,7 +1,19 @@
-import { Route } from '../types';
+import { Route, SubRoute } from '../types';
+
+const subItems: SubRoute[] = [
+  {
+    label: 'Despesas',
+    href: '/finance/expenses',
+  },
+  {
+    label: 'Receitas',
+    href: '/finance/revenues',
+  },
+]
 
 export const finance: Route = {
   icon: 'Money',
   label: 'Financeiro',
   href: '/finance',
+  subItems
 };
