4b118dbf854bdc40418f60b1cb96f55f91b495d4
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2024-12-04 19:42:57 -0300

    feat: pages

---
+++ src/app/(base-layout)/cells/page.tsx
@@ -0,0 +1,4 @@
+import styles from './CellsPage.module.sass';
+export default function Cells() {
+  return <h1 className={styles.container}>Cells</h1>;
+}
---
+++ src/app/(base-layout)/secretary/page.tsx
@@ -0,0 +1,4 @@
+import styles from './Secretary.module.sass';
+export default function Secretary() {
+  return <h1 className={styles.container}>Secretary</h1>;
+}
